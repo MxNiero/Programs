cfb00c1b623a9cb8b55c6a196cfe79d4adfabcb8
MxNiero <ananovvitaliy@gmail.com>
2024-08-04 18:21:21 +0200

    Third Commit

--- HomeWork1/HomeWork1_2.py
+++ HomeWork1/HomeWork1_2.py
@@ -3,12 +3,12 @@ user_input = int(input("Please write a 5 numbers: "))
 
 first_number = user_input // 10000
 
-second_number = (user_input % 10000) // 1000
+second_number = (user_input % 10000) // 1000 * 10
 
-third_number = (user_input % 1000) // 100
+third_number = (user_input % 1000) // 100 * 100
 
-fourth_number = (user_input % 100) // 10
+fourth_number = (user_input % 100) // 10 * 1000
 
-fifth_number = user_input % 10
+fifth_number = user_input % 10 * 10000
 
-print(str(fifth_number)+str(fourth_number)+str(third_number)+str(second_number)+str(first_number))
+print(fifth_number + fourth_number + third_number + second_number + first_number)
